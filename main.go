508e652957852e7f0aaad8d09d4d0d525efa36f9
shiatomaro <trishiagavile0911@mgmail.com>
2025-01-08 20:34:46 +0800

    added token for discord bot

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+// go get from .env file
+godotenv.Load()
+
+func main() {
+	token := os.Getenv("BOT_TOKEN")
+	// to print if there's a token
+	log.Println("the bot token is " + token)
+	sess, err := discordgo.New("Bot " + token)
+	if err != nil {
+		// to create error if there's not token
+		log.fatal(err)
+	}
+}
