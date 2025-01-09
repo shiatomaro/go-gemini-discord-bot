fd31f5af96f7329e4c56f8544fe5d53a28245280
shiatomaro <trishiagavile0911@mgmail.com>
2025-01-09 17:54:51 +0800

    added comments, deleted printing of discord bot

--- main.go
+++ main.go
@@ -7,18 +7,29 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-// go get from .env file
+// get from .env file.
 godotenv.Load()
 
-const prefix string = "/"
+const prefix string = "/goaskaway" // command prefix for bot.
+
 
 func main() {
+	// to load token from .env file.
 	token := os.Getenv("BOT_TOKEN")
-	// to print if there's a token
-	log.Println("the bot token is " + token)
+	if token == "" {
+		// to check if BOT_TOKEN is set in .env file.
+		log.Println("BOT_TOKEN not set in .env file. Set BOT_TOKEN in envirorment")
+	}
+
 	sess, err := discordgo.New("Bot " + token)
 	if err != nil {
 		// to create error if there's not token
-		log.fatal(err)
+		log.fatal("No discord token found, check .env file if BOT_TOKEN is set in envirorment.")
+	}
+}
+
+func sess.AddHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
 	}
 }
